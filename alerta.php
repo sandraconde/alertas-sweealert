9474ead8d560b507dfca112cf2c0adcbc39b4982
sandraconde <sandraconde@GitHub.com>
2020-10-31 10:14:59 -0500

    subiendo

---
+++ alerta.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+</head>
+<body>
+	<a href="index.php">regresar al index</a>
+
+	<script >
+		
+		 Swal.fire(
+   'cual es tu nombre?',
+   'estas seguro?',
+   'de la pregunta',
+  
+)
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+</head>
+<body>
+	
+	<a href="alerta.php">alerta 1</a><br>	
+	<a href="alerta2.php">alerta 2</a><br>
+	<a href="alerta3.php">alerta 3</a><br>
+	<a href="alerta4.php?id=1&name=hola&edad=12&opcion=si" title="">alerta 4</a>
+	<script >
+
+		/*
+		alert("hola soy sandra!");
+		Swal.fire('Any fool can use a computer');
+		 Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Your work has been saved',
+  showConfirmButton: false,
+  timer: 1500
+})
+	</script>
+</body>
+</html>
